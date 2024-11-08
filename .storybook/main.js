9012941f3dbd83ff76476be447cbdd895cd3f705
Jack Westbrook <jack.westbrook@gmail.com>
2024-11-08 15:44:52 +0100

    Wip

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: [
+    "../public",
+    {
+      from: "../some/directory/of/images/react-icon.svg",
+      to: "/assets/images/react-icon.svg",
+    },
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
